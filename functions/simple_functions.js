5b40048a7b9646b42ecf5fc92f3776e0197720e9
Taylor <4.alfred.taylor@gmail.com>
2024-04-30 14:09:56 -0400

    added simple functions js

---
+++ functions/simple_functions.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+function favoriteThings(){
+    console.log("Taylor");
+    console.log("Warm Bodies");
+    console.log("The Rare Occasions");
+}
+
+function whyImHere(){
+    console.log("I joined to jumpstart my career.");
+}
+
+function favoritePlace(){
+    console.log("My Favorite Place to visit at the moment is Chattanooga as the mountains are always so peaceful");
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
